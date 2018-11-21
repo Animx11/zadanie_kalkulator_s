b30a07498774abcec9576024079bce866c9fb041
Animx11 <animx11@gmail.com>
2018-11-21 22:48:47 +0100

    Making classes currency and rates to deserializing into them current value of currency.

---
+++ src/main/java/Spring/NbpEntities/Currency.java
@@ -0,0 +1,59 @@
+package Spring.NbpEntities;
+
+
+import java.util.List;
+
+public class Currency {
+
+    private String table;
+    private String currency;
+    private String code;
+    private List<Rates> rates;
+
+    //Constructors
+
+    public Currency() {
+    }
+
+    public Currency(String table, String currency, String code, List<Rates> rates) {
+        this.table = table;
+        this.currency = currency;
+        this.code = code;
+        this.rates = rates;
+    }
+
+    //Getters and setters
+
+    public String getTable() {
+        return table;
+    }
+
+    public void setTable(String table) {
+        this.table = table;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public List<Rates> getRates() {
+        return rates;
+    }
+
+    public void setRates(List<Rates> rates) {
+        this.rates = rates;
+    }
+}
+
---
+++ src/main/java/Spring/NbpEntities/Rates.java
@@ -0,0 +1,45 @@
+package Spring.NbpEntities;
+
+public class Rates {
+
+    private String no;
+    private String effectiveDate;
+    private float mid;
+
+    //Constructors
+
+    public Rates() {
+    }
+
+    public Rates(String no, String effectiveDate, float mid) {
+        this.no = no;
+        this.effectiveDate = effectiveDate;
+        this.mid = mid;
+    }
+
+    //Getters and setters
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+    public String getEffectiveDate() {
+        return effectiveDate;
+    }
+
+    public void setEffectiveDate(String effectiveDate) {
+        this.effectiveDate = effectiveDate;
+    }
+
+    public float getMid() {
+        return mid;
+    }
+
+    public void setMid(float mid) {
+        this.mid = mid;
+    }
+}
